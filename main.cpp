34449213868d8580b503ba9e7b45c57d233a78c5
Lukasz Drozd <luk4as66@gmail.com>
2012-08-09 20:35:11 +0200

    project setup in netBeans IDE

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: lukasz
+ *
+ * Created on 9 sierpień 2012, 20:16
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
