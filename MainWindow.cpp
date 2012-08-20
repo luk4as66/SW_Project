4b115f9c40dca7f4c28829373070c7c56b940af7
pablo <pablo.barczyk@gmail.com>
2012-08-20 19:36:18 +0200

    podczepiłem wyszukiwanie urządzen pod jakis tam przycisk w menu. Dodałem też nowe okno, które będzie pokazywać nazwy, oraz adresy znalezionych urządzen.  Jeszcze to dokoncze ale juz nie dzisiaj bo zaraz trzeba zapierdzielać do roboty na 8 godzin:)

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,21 +6,65 @@
  */
 
 #include "MainWindow.h"
+#include <QDialog>
+#include <QTextEdit>
+#include <fstream>
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
 
 
 MainWindow::MainWindow(QWidget *parent):QMainWindow(parent) {
+    /*
     QAction *quit = new QAction("&Find Devices...", this);
     QMenu *file;
     SearchDevicesLabel = new QLabel("Find Devices..");
     file = menuBar()->addMenu("&Connect");
     statusBar()->showMessage("QSimulate has started");
    
-    connect (quit, SIGNAL(triggered()), SearchDevicesLabel, SLOT(find()));  
+    //connect (quit, SIGNAL(triggered()), SearchDevicesLabel, SLOT(find()));  
+    connect(SearchDevicesLabel, SIGNAL(clicked()), this, SLOT(find()));
      file->addAction(quit);
-    resize(600,600);
+    resize(600,600);*/
+    SearchDevicesAction = new QAction(tr("&Search"), this);
+    fileMenu =  menuBar()->addMenu(tr("&File"));
+    
+    Exit = new QAction(tr("&Exit"), this);
+    ViewDevices = new QAction(tr("&View Devices"), this);
+    
+    
+    connect(SearchDevicesAction, SIGNAL(triggered()), this, SLOT(find()));
+    connect(Exit, SIGNAL(triggered()), qApp, SLOT(quit()));
+    connect(ViewDevices, SIGNAL(triggered()), this, SLOT(discovered()));
+
+    fileMenu->addAction(SearchDevicesAction);
+    fileMenu->addAction(Exit);
+    fileMenu->addAction(ViewDevices);
+    
+    
+    
+    this->resize(600,600);
 }
+   
 void MainWindow:: find(){
+ 
+ bluetooth search;
+ search.discover();
  statusBar()->showMessage("Searching for devices...");
+}
+
+void MainWindow :: discovered()
+{
+    //this->searchWindow = new QDialog();
+    //connect(searchWindow,SIGNAL(triggered()), this, SLOT(searched()));
+    //searchWindow->show();
+    
+    QTextEdit *discovered;
+    discovered = new QTextEdit();
+    
+    discovered->showNormal();
+    
 }
 MainWindow::~MainWindow() {
 }
